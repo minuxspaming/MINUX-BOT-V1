77ca9dd81b8cb9e9029f8621074bf4afee62fb7a
minuxspaming <hff48647@gmail.com>
2026-02-23 13:43:16 +0000

    Add files via upload

---
+++ plugins/owner.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+  let caption = `
+*「 معلومات عن صاحب البوت 」*\n\n
+*Telegram :*\n https://t.me/minux_rida\n
+*instagram:*\ninstagram.com/minux_rida
+
+
+
+*script bot :* ENJOY 
+
+`.trim()
+  m.reply(caption)
+}
+handler.help = ['owner']
+handler.tags = ['infobot']
+handler.command = /^(owner)$/i
+handler.limit = false
+
+export default handler
